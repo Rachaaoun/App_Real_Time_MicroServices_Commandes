ddcdfdf947ab2f83cd0e66d1b66beb10dff3b236
ahmed <mbenattiaa@gmail.com>
2025-10-31 14:05:01 +0100

    correction

--- backend/order-service/src/main/java/com/ecommerce/order/kafka/OrderProducer.java
+++ backend/order-service/src/main/java/com/ecommerce/order/kafka/OrderProducer.java
@@ -1,6 +1,7 @@
 package com.ecommerce.order.kafka;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,7 @@ import com.ecommerce.order.avro.Order;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
-@RestController
-@RequestMapping("/orders")
+@Service
 public class OrderProducer {
 
     private final KafkaTemplate<String, Order> kafkaTemplate;
@@ -20,23 +20,4 @@ public class OrderProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @PostMapping("/kafka/send")
-    public String sendOrder(@RequestBody Order order) {
-        kafkaTemplate.send(TOPIC, order.getOrderId().toString(), order);
-        return "Order sent to Kafka: " + order.getOrderId();
-    }
-        // Méthode pour envoyer un Order à Kafka
-    public void sendTestOrder(String orderId, String productName, double price) {
-        Order order = Order.newBuilder()
-                .setOrderId(orderId)
-                .setCustomerId("CUST-001")       // Valeur par défaut
-                .setProductId(productName)       // ici tu peux mettre productName
-                .setQuantity(1)
-                .setPrice(price)
-                .setOrderDate("2025-10-31")      // ou LocalDate.now().toString()
-                .build();
-
-        kafkaTemplate.send("orders", order.getOrderId().toString(), order);
-        System.out.println("✅ Order envoyé avec succès : " + order);
-    }
 }
